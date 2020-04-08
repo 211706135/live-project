29342780dc686eaeb1b8831050bb3e9068f37623
211706135 <60473632+211706135@users.noreply.github.com>
2020-04-08 16:41:01 +0800

    Update live-project.py

--- live-project.py
+++ live-project.py
@@ -1,4 +1,6 @@
 import numpy
+import time
+import operator
 def delete(arr,c):
     s=0
     if len(arr)==0 :
@@ -12,18 +14,7 @@ def delete(arr,c):
         return '还未添加此记录'
     else:
         return arr
-    
-import time
-import datetime
-s=[(211706127,'ts',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706129,'wcf',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706146,'yl',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706130,'wlp',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),]
-def deng(num,name):  
-    flag=0
+def sort1(s):
+    result = sorted(s,key=operator.itemgetter(0))
     for i in range(len(s)):
-        if s[i][0]==num:#判断列表里面有没有此学号
-            flag=1#如果有则变为1
-    if flag==0:#没有则添加
-        s.append((num,name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),))
-deng(211706126,'www')
+        print(result[i],'\n')
